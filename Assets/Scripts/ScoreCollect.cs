b77477a18cc0f1b298ca0f0d27e28d2a78f1b60b
utsubyo <97504303+15-junyuan@users.noreply.github.com>
2022-02-08 14:12:00 +0800

    add unity and pic
    add unity and image

---
+++ Assets/Scripts/ScoreCollect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCollect : MonoBehaviour
+{
+
+    public Text Scoretxt;
+    float Scorevalue = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scoretxt.text = "Score: " + Scorevalue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Obstacle")
+        {
+            Scorevalue++;
+            Scoretxt.text = "Score: " + Scorevalue;
+            Destroy(other.gameObject);
+        }
+    }
+}
